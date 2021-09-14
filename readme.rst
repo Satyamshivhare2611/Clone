3af6b1e6ecc2932acaf32a7364cb82234f36d2f3
Satyam Shivhare <satyamshivhare2611@gmail.com>
2021-09-14 22:10:59 +0530

    Clone Commit

---
+++ readme.rst
@@ -0,0 +1,71 @@
+blurb_it
+--------
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+    :target: https://github.com/ambv/black
+
+.. image:: https://github.com/python/blurb_it/actions/workflows/ci.yml/badge.svg?event=push
+    :target: https://github.com/python/blurb_it/actions
+
+.. image:: https://codecov.io/gh/python/blurb_it/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/python/blurb_it
+
+
+``blurb add`` over the internet.
+
+About
+=====
+
+📜🤖 blurb-it allows you to add a misc/news file to your own
+`CPython <https://github.com/python/cpython>`_ pull request.
+
+Misc/News file `is needed <https://devguide.python.org/committing/#what-s-new-and-news-entries>`_ for
+almost all non-trivial changes to CPython.
+
+To use blurb-it, you must be logged in to GitHub.
+
+Install blurb-it GitHub App to your account, and then grant the ``write`` access to your
+fork of the CPython repository.
+
+Since blurb-it will have write access to the granted repo, you should only install
+it on your own CPython repository.
+
+`Install blurb-it <https://github.com/apps/blurb-it/installations/new>`_ .
+
+Uninstall blurb-it
+==================
+
+1. Go to https://github.com/settings/installations.
+
+2. Click blurb-it's "Configure" button.
+
+3. Scroll down and click the "Uninstall" button.
+
+Deploy
+======
+
+|Deploy|
+
+.. |Deploy| image:: https://www.herokucdn.com/deploy/button.svg
+   :target: https://heroku.com/deploy?template=https://github.com/python/blurb_it
+
+
+Requirements and dependencies
+=============================
+
+- Python 3.7+
+- aiohttp
+- aiohttp-jinja2
+- gidgethub >= 5.0.0
+- pyjwt >= 2.0.0
+- cryptography
+
+
+Running Tests
+=============
+
+1. Install pyhon virtual environment with `$ python3 -m venv venv`.
+2. Activate virtual environment with `$ . venv/bin/activate`.
+3. Install dev-requirements with `(venv)$ pip install -r dev-requirements.txt`.
+4. Run all test with `(venv)$ pytest tests`.
+
---
+++ tests/test_util.py
@@ -0,0 +1,87 @@
+import string
+
+import pytest
+
+from blurb_it import util
+from aiohttp.test_utils import make_mocked_request
+from aiohttp_session import (Session, SESSION_KEY)
+
+
+async def test_nonceify():
+    body = (
+        "Lorem ipsum dolor amet flannel squid normcore tbh raclette enim"
+        "pabst tumblr wolf farm-to-table bitters. Bitters keffiyeh next"
+        "level proident normcore, et all of +1 90's in blue bottle"
+        "chillwave lorem. Id keffiyeh microdosing cupidatat pour-over"
+        "paleo farm-to-table tumeric sriracha +1. Raclette in poutine,"
+        "bushwick kitsch id pariatur hexagon. Thundercats shaman beard,"
+        "nulla swag echo park organic microdosing. Hot chicken tbh pop-up"
+        "tacos, asymmetrical tilde veniam bespoke reprehenderit ut do."
+    )
+
+    nonce = await util.nonceify(body)
+    assert nonce == "Ps4kgC"
+
+
+async def test_get_misc_news_filename():
+    path = await util.get_misc_news_filename(
+        bpo=123,
+        section="Library",
+        body="Lorem ipsum dolor amet flannel squid normcore tbh raclette enim"
+        "pabst tumblr wolf farm-to-table bitters. Bitters keffiyeh next"
+        "level proident normcore, et all of +1 90's in blue bottle"
+        "chillwave lorem. Id keffiyeh microdosing cupidatat pour-over"
+        "paleo farm-to-table tumeric sriracha +1. Raclette in poutine,"
+        "bushwick kitsch id pariatur hexagon. Thundercats shaman beard,"
+        "nulla swag echo park organic microdosing. Hot chicken tbh pop-up"
+        "tacos, asymmetrical tilde veniam bespoke reprehenderit ut do.",
+    )
+
+    assert path.startswith("Misc/NEWS.d/next/Library/")
+    assert path.endswith(".bpo-123.Ps4kgC.rst")
+
+
+async def test_has_session():
+    request = mock_request_session()
+
+    has_session = await util.has_session(request)
+    assert has_session
+
+
+async def test_session_context():
+    request = mock_request_session()
+
+    session_context = await util.get_session_context(request)
+    assert session_context == {"username": "blurb", "token": "124"}
+
+
+def mock_request_session():
+    request = make_mocked_request("GET", "/")
+    session = Session("identity", data=None, new=False)
+    session["token"] = "124"
+    session["username"] = "blurb"
+    request[SESSION_KEY] = session
+
+    return request
+
+
+def test_get_csrf_token__not_existing(mocker):
+    mocker.patch("blurb_it.util.create_csrf_token", return_value="foobar")
+
+    assert util.get_csrf_token({}) == "foobar"
+
+
+def test_get_csrf_token__existing():
+
+    assert util.get_csrf_token({"csrf": "foobar"}) == "foobar"
+
+
+def test_create_csrf_token():
+    token = util.create_csrf_token()
+    assert len(token) == 43
+    assert set(token) <= set(string.ascii_letters + string.digits + "-_=")
+
+
+@pytest.mark.parametrize("token, match", [("a", True), ("b", False)])
+def test_compare_csrf_tokens__match(token, match):
+    assert util.compare_csrf_tokens("a", token) is match
